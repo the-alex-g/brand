c43f18bc5c00e93283074148580aec41624366dc
Alex G <alex.gestwicki@gmail.com>
2024-05-11 21:33:09 -0400

    Improve code and add version numbers

--- brand_compiler.py
+++ brand_compiler.py
@@ -1,18 +1,34 @@
 import os
 
 
-def get_file_contents(filename):
-    lines = open(filename).readlines()
+def get_file_contents(path):
+    lines = open(path).readlines()
     string = ""
     for line in lines:
         string += line
     return string
 
 
-def compile():
-    available_modules = []
+def comma_separate(stuff):
+    string = ""
+    for item in stuff:
+        if string != "":
+            string += ", "
+        string += item
+    return string
 
+
+def get_module_section(module, section):
+    path = os.path.join(os.getcwd(), "brand_" + module, section + ".txt")
+    if os.path.isfile(path):
+        return get_file_contents(path)
+    return ""
+
+
+def get_available_modules():
+    available_modules = []
     cwd = os.getcwd()
+
     for item in os.listdir(cwd):
         path = os.path.join(cwd, item)
         if os.path.isdir(path):
@@ -20,18 +36,22 @@ def compile():
                 available_modules.append(item[6:])
     available_modules.sort()
 
-    module_string = ""
-    for module in available_modules:
-        if module_string != "":
-            module_string += ", "
-        module_string += module
+    return available_modules
 
-    print("The following brand modules are available:", module_string)
+
+
+def compile():
+    available_modules = get_available_modules()
+
+    print("The following brand modules are available:", comma_separate(available_modules))
 
     module_name = ""
     while True:
         module_name = input("Enter the desired module: ")
         if module_name in available_modules:
+            break
+        elif module_name == "":
+            print("compiling core module")
             break
         else:
             print("That is not a valid module name.")
@@ -43,14 +63,25 @@ def compile():
 
 
     brand_file = open("brand.py", "w")
+    # compile module imports
     for module in modules_to_compile:
-        brand_file.write(get_file_contents(os.path.join(cwd, "brand_" + module, "imports.txt")))
+        brand_file.write(get_module_section(module, "imports"))
+    # add a line of empty space
     brand_file.write("\n\n")
+    # compile module global variables
     for module in modules_to_compile:
-        brand_file.write(get_file_contents(os.path.join(cwd, "brand_" + module, "consts.txt")))
+        brand_file.write(get_module_section(module, "globals"))
+    # add two empty lines
     brand_file.write("\n\n\n")
+    # compile module bodies
     for module in modules_to_compile:
-        brand_file.write(get_file_contents(os.path.join(cwd, "brand_" + module, "body.txt")))
+        brand_file.write(get_module_section(module, "body"))
+    
+    brand_file.write("\n\n# using modules ")
+    for module in modules_to_compile:
+        brand_file.write(module + " " + get_module_section(module, "version"))
+        if module != modules_to_compile[-1]:
+            brand_file.write(", ")
 
 
 compile()
