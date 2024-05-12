75103cadef9954dac2faf30406bf5e5ae766990f
Alex G <alex.gestwicki@gmail.com>
2024-05-12 12:26:37 -0400

    Add command to build all modules

--- brand_compiler.py
+++ brand_compiler.py
@@ -39,49 +39,57 @@ def get_available_modules():
     return available_modules
 
 
-
-def compile():
+def get_module_name():
     available_modules = get_available_modules()
 
     print("The following brand modules are available:", comma_separate(available_modules))
 
-    module_name = ""
     while True:
         module_name = input("Enter the desired module: ")
-        if module_name in available_modules:
-            break
+        if module_name in available_modules or module_name == "all":
+            return module_name
         elif module_name == "":
             print("compiling core module")
-            break
+            return module_name
         else:
             print("That is not a valid module name.")
 
+
+
+def compile(module_name="", file_name="brand"):
+    if module_name == "":
+        module_name = get_module_name()
+    if module_name == "all":
+        for module in get_available_modules():
+            compile(module_name=module, file_name="brand_" + module)
+        return
 
     modules_to_compile = ["core"]
     if module_name != "core":
         modules_to_compile.append(module_name)
 
-
-    brand_file = open("brand.py", "w")
+    brand_file = open(file_name + ".py", "w")
     # compile module imports
     for module in modules_to_compile:
-        brand_file.write(get_module_section(module, "imports"))
+        brand_file.write(get_module_section(module, "imports") + "\n")
     # add a line of empty space
-    brand_file.write("\n\n")
+    brand_file.write("\n")
     # compile module global variables
     for module in modules_to_compile:
-        brand_file.write(get_module_section(module, "globals"))
+        brand_file.write(get_module_section(module, "globals") + "\n")
     # add two empty lines
-    brand_file.write("\n\n\n")
+    brand_file.write("\n\n")
     # compile module bodies
     for module in modules_to_compile:
-        brand_file.write(get_module_section(module, "body"))
+        brand_file.write(get_module_section(module, "body") + "\n\n\n")
     
-    brand_file.write("\n\n# using modules ")
+    brand_file.write("# using modules: ")
     for module in modules_to_compile:
         brand_file.write(module + " " + get_module_section(module, "version"))
         if module != modules_to_compile[-1]:
             brand_file.write(", ")
+    
+    brand_file.close()
 
 
 compile()
